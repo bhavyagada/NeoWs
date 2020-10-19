ad022b011d0a186759512c386fb5ca21451f8fbd
Bhavya <bhavyasgada@gmail.com>
2020-10-20 01:36:26 +0530

    Vuetify Setup

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,19 @@
 <template>
-  <div id="app">
-    <NavBar />
-  </div>
+  <v-app>
+    <v-main>
+      <NavBar />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import NavBar from '@/components/NavBar'
+import NavBar from './components/NavBar'
 
 export default {
-  name: 'app',
+  name: 'App',
+
   components: {
     NavBar
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #012;
-  text-transform: uppercase;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import firebase from './components/firebaseConfig'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
@@ -12,6 +13,7 @@ firebase.auth().onAuthStateChanged(() => {
     app = new Vue({
       router,
       store,
+      vuetify,
       render: h => h(App)
     }).$mount('#app')
   }
